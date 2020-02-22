bd7e527bc4965de07c54d316853308d8ec1b184c
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-02-22 10:43:23 +0100

    b2v

--- setup.py
+++ setup.py
@@ -2,15 +2,12 @@ from setuptools import setup
 import ast
 import sys
 
-setup_requires = ['setuptools >= 30.3.0', 'better-setuptools-git-version']
+setup_requires = ['setuptools >= 30.3.0']
 if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
     setup_requires.append('pytest-runner')
 
 
 setup(description="pylogstash",
       long_description=open('README.md').read(),
-      version_config=dict(
-        version_format='{tag}.dev{sha}', 
-        starting_version="0.1.0",
-      ),
+      version_config="0.1.2",
       setup_requires=setup_requires)
