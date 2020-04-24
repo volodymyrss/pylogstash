c6dfc07170e5d551785ebec23c05d08f75f40dad
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-04-24 10:15:59 +0200

    Bump version: 0.1.8 → 0.1.9

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
 
 setup(description="pylogstash",
       long_description=open('README.md').read(),
-      version="0.1.8",
+      version="0.1.9",
       setup_requires=setup_requires)
