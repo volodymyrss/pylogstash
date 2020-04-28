edeb81f99b8d868a321f78e438cfcfad701d192f
Volodymyr SAVCHENKO <contact@volodymyrsavchenko.com>
2020-04-28 11:02:55 +0200

    Bump version: 0.1.11 → 0.1.12

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
 
 setup(description="pylogstash",
       long_description=open('README.md').read(),
-      version="0.1.11",
+      version="0.1.12",
       setup_requires=setup_requires)
