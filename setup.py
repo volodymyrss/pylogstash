d1e20ed517cdbd7b945aa6da8796a6ca82774c84
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2020-11-18 20:09:52 +0100

    Bump version: 0.1.14 → 0.1.15

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
 
 setup(description="pylogstash",
       long_description=open('README.md').read(),
-      version="0.1.14",
+      version="0.1.15",
       setup_requires=setup_requires)
