6beb213bda1832951ecf955adfeae3843cb0eb4f
Volodymyr Savchenko <Volodymyr.Savchenko@unige.ch>
2021-01-20 11:46:56 +0100

    Bump version: 0.1.16 → 0.1.17

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
 
 setup(description="pylogstash",
       long_description=open('README.md').read(),
-      version="0.1.16",
+      version="0.1.17",
       setup_requires=setup_requires)
