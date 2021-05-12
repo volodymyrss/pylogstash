13615d6defba5aa4ec674a5be334e5961299134e
Volodymyr Savchenko <contact@volodymyrsavchenko.com>
2021-05-12 13:42:56 +0200

    send strings and bytes

--- setup.py
+++ setup.py
@@ -9,5 +9,5 @@ if {'pytest', 'test', 'ptr'}.intersection(sys.argv):
 
 setup(description="pylogstash",
       long_description=open('README.md').read(),
-      version="0.1.18",
+      version="0.1.17",
       setup_requires=setup_requires)
